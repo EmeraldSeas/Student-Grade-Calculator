8d427a0f605c4704777c5bb3c36cd7da6397d29c
EmeraldSeas <115032064+EmeraldSeas@users.noreply.github.com>
2023-07-19 11:45:02 -0400

    Initial commit.

---
+++ Student Grade Calculator/Program.cs
@@ -0,0 +1,9 @@
+﻿using Student_Grade_Calculator;
+
+Student sneed = new Student("Sneed", 82, 66, 89, 91, 100);
+Student jimbo = new Student("Jimbo", 52, 81, 99, 58, 88);
+Student chuck = new Student("Chuck", 98, 90, 87, 82, 99);
+
+Console.WriteLine($"Student name: {sneed.name}, ID: {sneed.id}, final grade: {sneed.finalScore}, letter grade: {sneed.letterScore}");
+Console.WriteLine($"Student name: {jimbo.name}, ID: {jimbo.id}, final grade: {jimbo.finalScore}, letter grade: {jimbo.letterScore}");
+Console.WriteLine($"Student name: {chuck.name}, ID: {chuck.id}, final grade: {chuck.finalScore}, letter grade: {chuck.letterScore}");
---
+++ Student Grade Calculator/Student.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Grade_Calculator
+{
+    internal class Student
+    {
+        public int id { get; }
+        public string name { get; }
+        public int englishScore { get; }
+        public int algebraScore { get; }
+        public int biologyScore { get; }
+        public int csScore { get; }
+        public int psychologyScore { get; }
+        public int finalScore { get; }
+        public string letterScore { get; }
+
+
+        public Student(string name, int englishScore, int algebraScore, int biologyScore, int csScore, int psyschologyScore)
+        {
+            this.id = new Random().Next(10000, 99999);
+            this.name = name;
+            this.englishScore = englishScore;
+            this.algebraScore = algebraScore;
+            this.biologyScore = biologyScore;
+            this.csScore = csScore;
+            this.psychologyScore = psyschologyScore;
+            this.finalScore = calcScore(this.englishScore, this.algebraScore, this.biologyScore, this.csScore, this.psychologyScore);
+
+
+            switch (this.finalScore)
+            {
+                case >= 90:
+                    this.letterScore = "A";
+                    break;
+                case >= 80:
+                    this.letterScore = "B";
+                    break;
+                case >= 70:
+                    this.letterScore = "C";
+                    break;
+                case >= 60:
+                    this.letterScore = "D";
+                    break;
+                case < 60:
+                    this.letterScore = "F";
+                    break;
+                default:
+                    this.letterScore = "Error";
+                    break;
+            }
+        }
+
+        public int calcScore(int englishScore, int algebraScore, int biologyScore, int csScore, int psychologyScore)
+        {
+            int eValue = englishScore * 3;
+            int aValue = algebraScore * 3;
+            int bValue = biologyScore * 4;
+            int cValue = csScore * 4;
+            int pValue = psychologyScore * 3;
+            return (eValue + aValue + bValue + cValue + pValue) / 17;
+        }
+    }
+}
