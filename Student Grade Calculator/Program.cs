bb6cec1bd504ec3463aaef440870478f5f908fbf
EmeraldSeas <115032064+EmeraldSeas@users.noreply.github.com>
2023-07-21 06:21:30 -0400

    remove unused use statements and apply naming suggestions

--- Student Grade Calculator/Program.cs
+++ Student Grade Calculator/Program.cs
@@ -4,9 +4,9 @@ Student sneed = new Student("Sneed", new StudentClasses("Biology I", 89, 3), new
 Student jimbo = new Student("Jimbo", new StudentClasses("Biology I", 76, 3), new StudentClasses("Computer Science", 88, 4));
 Student chuck = new Student("Chuck", new StudentClasses("Biology I", 82, 3), new StudentClasses("Computer Science", 91, 4));
 
-Console.WriteLine($"Student name: {sneed.name}, ID: {sneed.id}, Final grade: {sneed.finalScore}, Letter grade: {sneed.letterScore}");
-sneed.toString();
-Console.WriteLine($"Student name: {jimbo.name}, ID: {jimbo.id}, Final grade: {jimbo.finalScore}, Letter grade: {jimbo.letterScore}");
-jimbo.toString();
-Console.WriteLine($"Student name: {chuck.name}, ID: {chuck.id}, Final grade: {chuck.finalScore}, Letter grade: {chuck.letterScore}");
-jimbo.toString();
+Console.WriteLine($"Student Name: {sneed.Name}, ID: {sneed.Id}, Final Grade: {sneed.FinalScore}, Letter Grade: {sneed.LetterScore}");
+sneed.PrintString();
+Console.WriteLine($"Student Name: {jimbo.Name}, ID: {jimbo.Id}, Final Grade: {jimbo.FinalScore}, Letter Grade: {jimbo.LetterScore}");
+jimbo.PrintString();
+Console.WriteLine($"Student Name: {chuck.Name}, ID: {chuck.Id}, Final Grade: {chuck.FinalScore}, Letter Grade: {chuck.LetterScore}");
+jimbo.PrintString();
--- Student Grade Calculator/Student.cs
+++ Student Grade Calculator/Student.cs
@@ -1,73 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Student_Grade_Calculator
+﻿namespace Student_Grade_Calculator
 {
     internal class Student
     {
-        internal int id { get; }
-        internal string name { get; }
-        internal int finalScore { get; }
-        internal string letterScore { get; }
+        internal int Id { get; }
+        internal string Name { get; }
+        internal int FinalScore { get; }
+        internal string LetterScore { get; }
         private List<StudentClasses> classList = new List<StudentClasses>(); // list to keep track of a student's classes
         
         public Student(string name, params StudentClasses[] newClass) // student constructor that accepts an arbitrary number of StudentClasses objects
         {
-            id = new Random().Next(10000, 99999);
-            this.name = name;
+            Id = new Random().Next(10000, 99999);
+            this.Name = name;
             foreach (StudentClasses newClasses in newClass)
             {
                 classList.Add(newClasses);
             }
             
-            finalScore = calcScore();
+            FinalScore = CalcScore();
             
-            switch (this.finalScore) // converts student's final numerical grade to a letter grade
+            switch (this.FinalScore) // converts student's final numerical Grade to a letter Grade
             {
                 case >= 90:
-                    this.letterScore = "A";
+                    this.LetterScore = "A";
                     break;
                 case >= 80:
-                    this.letterScore = "B";
+                    this.LetterScore = "B";
                     break;
                 case >= 70:
-                    this.letterScore = "C";
+                    this.LetterScore = "C";
                     break;
                 case >= 60:
-                    this.letterScore = "D";
+                    this.LetterScore = "D";
                     break;
                 case < 60:
-                    this.letterScore = "F";
+                    this.LetterScore = "F";
                     break;
                 default:
-                    this.letterScore = "Error";
+                    this.LetterScore = "Error";
                     break;
             }
         }
 
-        public void addClass(StudentClasses newStudentClass) // method to add classes to an already existing student's list
+        public void AddClass(StudentClasses newStudentClass) // method to add classes to an already existing student's list
         {
             classList.Add(newStudentClass);
         }
 
-        public int calcScore() // calculate total score by iterating through a student's classList
+        private int CalcScore() // calculate total score by iterating through a student's classList
         {
-            int GPA = 0;
+            int gpa = 0;
             int totalCredits = 0;
             foreach (StudentClasses classScore in classList) // iterates through the classList
             {
-                GPA += classScore.grade * classScore.credits;
-                totalCredits += classScore.credits;
+                gpa += classScore.Grade * classScore.Credits;
+                totalCredits += classScore.Credits;
             }
 
             if (totalCredits > 0) // simple if/else to prevent any division by zero
             {
-                return GPA / totalCredits;
+                return gpa / totalCredits;
             }
             else
             {
@@ -75,26 +67,26 @@ namespace Student_Grade_Calculator
             }
         }
 
-        public void toString() // prints out a student's classes with respective grades and credit weights
+        public void PrintString() // prints out a student's classes with respective grades and credit weights
         {
             foreach (StudentClasses curClass in classList)
             {
-                Console.WriteLine($"Class name: {curClass.name}, Current grade: {curClass.grade}, Credit weight: {curClass.credits}");
+                Console.WriteLine($"Class Name: {curClass.Name}, Current Grade: {curClass.Grade}, Credit Weight: {curClass.Credits}");
             }
         }
     }
     
-    internal class StudentClasses // class object for keeping a class' name, grade value, and credit weight
+    internal class StudentClasses // class object for keeping a class' Name, Grade value, and credit weight
     {
-        internal string name { get; }
-        internal int grade { get; }
-        internal int credits { get; }
+        internal string Name { get; }
+        internal int Grade { get; }
+        internal int Credits { get; }
 
         public StudentClasses(string name, int grade, int credits)
         {
-            this.name = name;
-            this.grade = grade;
-            this.credits = credits;
+            this.Name = name;
+            this.Grade = grade;
+            this.Credits = credits;
         }
     }
 }
